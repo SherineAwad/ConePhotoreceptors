3d32bee0de4734570f0603d3f7b3efb7e61e5672
Sherine Awad <sherinem@gl3030.arc-ts.umich.edu>
2024-11-18 11:08:47 -0500

    add more plots

--- geneExp.R
+++ geneExp.R
@@ -26,16 +26,17 @@ myObject[["cells"]] <- Idents(object = myObject)
 
 DEGs <- FindAllMarkers(object = myObject, only.pos = TRUE, min.pct = 0.1,test.use ='wilcox', logfc.threshold = 0.5)
 
-top_genes <- DEGs[order(DEGs$p_val), ][1:50, ]  # Top 10 genes based on smallest p-value
-top_gene_list <- rownames(top_genes)
+top_genes<- DEGs%>% group_by(cluster) %>% top_n(n = 20, wt = avg_log2FC)
+top_gene_list <- top_genes$gene
+
+
 
 top_gene_list 
 
-mapal <- colorRampPalette(RColorBrewer::brewer.pal(11,"PuBu"))(256)
 figure_name <- ""
 figure_name <- paste(mysample, "Cellsheatmap.pdf", sep="")
 pdf(file =figure_name, width=18)
-DoHeatmap(object = myObject, features = top_gene_list,size = 2.5, group.by ="cells",  slot="data",group.colors = brewer.pal(9, "Blues")) +  scale_fill_gradientn(colors = c("blue", "white", "red")) #+ scale_fill_gradientn(colours = rev(mapal))
+DoHeatmap(object = myObject, features = top_gene_list,size = 2.5, group.by ="cells",  slot="data",group.colors = brewer.pal(9, "Blues")) +  scale_fill_gradientn(colors = c("blue", "white", "red")) 
 dev.off()
 
 myRDS <- paste(mysample, "_annotated.rds", sep="")
